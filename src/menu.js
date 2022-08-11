8d023bee798a4470137f62096c30cdd088f6fcc8
leonardo <leonardommascarenhas@gmail.com>
2022-08-10 21:00:24 -0300

    Menu changes and clean code

--- src/menu.js
+++ src/menu.js
@@ -1,51 +1,56 @@
 export default function createMenu(append) {
   append.innerHTML = "";
   let menuContainer = document.createElement("div");
-  menuContainer.classList.add("recipes");
-  let menu = document.createElement("div");
-  menu.classList.add("menu");
-  let menuItem1 = document.createElement("div");
-  menuItem1.classList.add("menu-item");
-  menuItem1.innerHTML = "Teste";
-  let menuItem2 = document.createElement("div");
-  menuItem2.classList.add("menu-item");
-  menuItem2.innerHTML = "Teste";
-  let menuItem3 = document.createElement("div");
-  menuItem3.classList.add("menu-item");
-  menuItem3.innerHTML = "Teste";
-  let menuItem4 = document.createElement("div");
-  menuItem4.classList.add("menu-item");
-  menuItem4.innerHTML = "Teste";
-  let menuItem5 = document.createElement("div");
-  menuItem5.classList.add("menu-item");
-  menuItem5.innerHTML = "Teste";
-  let menuItem6 = document.createElement("div");
-  menuItem6.classList.add("menu-item");
-  menuItem6.innerHTML = "Teste";
-  let menuItem7 = document.createElement("div");
-  menuItem7.classList.add("menu-item");
-  menuItem7.innerHTML = "Teste";
-  let menuItem8 = document.createElement("div");
-  menuItem8.classList.add("menu-item");
-  menuItem8.innerHTML = "Teste";
-  let menuItem9 = document.createElement("div");
-  menuItem9.classList.add("menu-item");
-  menuItem9.innerHTML = "Teste";
-  let menuItem10 = document.createElement("div");
-  menuItem10.classList.add("menu-item");
-  menuItem10.innerHTML = "Teste";
-  menuContainer.append(
-    menu,
-    menuItem1,
-    menuItem2,
-    menuItem3,
-    menuItem4,
-    menuItem5,
-    menuItem6,
-    menuItem7,
-    menuItem8,
-    menuItem9,
-    menuItem10
+  menuContainer.classList.add("menu-container");
+  createMenuItem(
+    menuContainer,
+    "../dist/img/menu/whisky.png",
+    "Whisky",
+    "A delicious whisky with a touch of spice"
+  );
+  createMenuItem(
+    menuContainer,
+    "../dist/img/menu/grapes.png",
+    "Wine Keg",
+    "A giant keg of wine with a touch of spice"
+  );
+  createMenuItem(
+    menuContainer,
+    "../dist/img/menu/vodka.png",
+    "Vodka",
+    "A bottle of vodka with a touch of spice"
+  );
+  createMenuItem(
+    menuContainer,
+    "../dist/img/menu/beer.png",
+    "Beer",
+    "A bottle of beer with a touch of spice"
+  );
+  createMenuItem(
+    menuContainer,
+    "../dist/img/menu/martini.png",
+    "Martini",
+    "A bottle of martini with a touch of spice"
+  );
+  createMenuItem(
+    menuContainer,
+    "../dist/img/menu/softdrinks.png",
+    "Soda",
+    "A delicious soda with a touch of spice"
   );
   append.appendChild(menuContainer);
+}
+
+function createMenuItem(append, image, title, description) {
+  let menuItemContainer = document.createElement("div");
+  menuItemContainer.classList.add("menu-item-container");
+  let menuImage = document.createElement("img");
+  let menuTitle = document.createElement("h2");
+  let menuDescription = document.createElement("p");
+  menuImage.src = image;
+  menuImage.classList.add("menuImage");
+  menuTitle.innerHTML = title;
+  menuDescription.innerHTML = description;
+  menuItemContainer.append(menuImage, menuTitle, menuDescription);
+  append.appendChild(menuItemContainer);
 }
